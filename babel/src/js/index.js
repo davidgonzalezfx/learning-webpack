ed4b984f32071bb01b3bfe85805f29cb18a69ff8
davidgonzalezfx <dvdramos16@gmail.com>
2021-02-05 17:46:57 -0500

    Feature - Babel

---
+++ babel/src/js/index.js
@@ -0,0 +1,13 @@
+import '../css/index.css'
+import search from './search'
+import render from './render'
+
+const id = prompt('Id de pokemon')
+
+search(id)
+  .then(data => {
+    render(data)
+  })
+  .catch(() => {
+    console.log('No hubo pokemon')
+  })
---
+++ babel/src/js/text.js
@@ -0,0 +1,5 @@
+const text = () => {
+  document.body.innerHTML = '<p>Si ves esto por 1000seg pasan cosas raras</p>'
+}
+
+export default text
