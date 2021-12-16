e4a0b2eb2ef4fb2c24f06058aae5f94e2a2d654c
davidgonzalez <david.gonzalez.-nd@disney.com>
2021-12-16 11:44:39 -0500

    Feature - Add Css loader and Scss support

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 /** @type {import('webpack').Configuration} */
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: 'main.js'
   },
   resolve: {
-    extensions: ['.js'],
+    extensions: ['.js']
   },
   module: {
     rules: [
@@ -18,16 +19,21 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-        },
+          loader: 'babel-loader'
+        }
       },
-    ],
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
       template: './public/index.html',
-      filename: './index.html',
+      filename: './index.html'
     }),
-  ],
-};
+    new MiniCssExtractPlugin()
+  ]
+}
