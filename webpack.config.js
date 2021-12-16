63f6f8903912246ec3e79eec226c0e7e1027552d
davidgonzalez <david.gonzalez.-nd@disney.com>
2021-12-16 14:20:34 -0500

    Feature - Minification and hashes

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,14 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   resolve: {
@@ -36,7 +37,7 @@ module.exports = {
         test: /\.(woff|woff2)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/fonts/[hash][ext][query]'
+          filename: 'assets/fonts/[contenthash][ext][query]'
         }
       }
     ]
@@ -47,6 +48,15 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html'
     }),
-    new MiniCssExtractPlugin(),
-  ]
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin()
+    ]
+  }
 }
